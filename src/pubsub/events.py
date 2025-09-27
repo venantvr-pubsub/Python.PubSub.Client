dafc19549b0840d9b4b869b547890f037aa4433d
rvv <r.venant.valery@gmail.com>
2025-09-27 22:33:27 +0200

    Removed @dataclass on events...

--- src/pubsub/events.py
+++ src/pubsub/events.py
@@ -1,14 +1,12 @@
-from dataclasses import dataclass
-
 from pydantic import BaseModel
 
 
-@dataclass  # (frozen=True)
+# @dataclass(frozen=True)
 class AllProcessingCompleted(BaseModel):
     payload: bool = True
 
 
-@dataclass  # (frozen=True)
+# @dataclass(frozen=True)
 class WorkerFailed(BaseModel):
     worker_name: str
     reason: str
